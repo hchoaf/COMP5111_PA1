20f7aad044e142c33c8e93b982709eb5f97f0a50
Hangsun Cho <hchoaf@connect.ust.hk>
2023-05-07 01:00:28 +0800

    create refined test evosuite0

---
+++ src/test/refined-evosuite0/comp5111/assignment/cut/Refined_Subject_FaultRevealing0_ESTest.java
@@ -0,0 +1,14 @@
+package comp5111.assignment.cut;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Refined_Subject_FaultRevealing0_ESTest extends Subject_FaultRevealing0_ESTest {
+
+	@Test
+	public void test01() {
+		fail("Not yet implemented");
+	}
+
+}
